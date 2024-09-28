f6158ef45e5cd5853127b7f6cdd7585bb317604f
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-28 22:25:36 +0900

    feat: interceptor 수정

--- nest-practice/src/common/interceptor/success.interceptor.ts
+++ nest-practice/src/common/interceptor/success.interceptor.ts
@@ -1,9 +1,16 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class SuccessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    console.log('Before... (Pre-controller)');
+    return next.handle().pipe(map((data) => ({ sucess: true, data: data })));
   }
 }
