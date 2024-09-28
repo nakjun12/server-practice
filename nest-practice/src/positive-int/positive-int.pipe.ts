33b799a531e62ccf539a1196d87fac4e3832e174
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-28 20:43:29 +0900

    feat: filter, pipe, middleware 추가

---
+++ nest-practice/src/positive-int/positive-int.pipe.ts
@@ -0,0 +1,8 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value: number, metadata: ArgumentMetadata) {
+    return value;
+  }
+}
