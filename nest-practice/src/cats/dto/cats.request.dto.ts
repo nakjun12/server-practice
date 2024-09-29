48d867685d1dfc6b2f194aa1f96fc449e0d2d309
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-29 10:28:32 +0900

    refactor: schema 활용하여 dto 정의

--- nest-practice/src/cats/dto/cats.request.dto.ts
+++ nest-practice/src/cats/dto/cats.request.dto.ts
@@ -1,15 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { OmitType } from '@nestjs/swagger';
+import { Cat } from '../cats.schema';
 
-export class CatRequestDto {
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-
-  @IsString()
-  @IsNotEmpty()
-  password: string;
-
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-}
+export class CatRequestDto extends OmitType(Cat, ['readOnlyData'] as const) {}
